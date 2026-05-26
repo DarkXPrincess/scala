3d7a202306dff8aaa2291e893a1be84cafd1e3ed
veronica <veronica.granados@correounivalle.edu.co>
2026-05-26 11:30:22 -0500

    definicion de funciones

---
+++ src/main/scala/Canicas/package.scala
@@ -0,0 +1,28 @@
+//1.1 Las canicas y los frascos
+package object Canicas {
+  type Frasco = (Int, Int)
+  type Distr = List[Frasco]
+
+//1.1.1 Canicas posibles en un frasco
+  def canicasPosiblesFrasco(f: Int, c:Int):List[Frasco]={
+
+  }
+
+  //1.1.2 Canicas posibles por frasco
+  def canicasPorFrasco(n:Int, c:Int):List[Distr]={
+
+  }
+
+  //1.1.3 Combinaciones de canicas por frasco
+  def mezclarLCanicas(lc : List [ Distr ]): List [ Distr ] ={
+
+  }
+  //1.1.4 Calculando las distribuciones requeridas
+  def distribucion(m:Int , n:Int , c:Int ): List [ Distr ] = {
+
+  }
+  //1.2 Calculo de posibles agrupaciones crecientes
+  def agrupaciones(m: Int ): List [ List [ Int ]] ={
+
+  }
+  }
